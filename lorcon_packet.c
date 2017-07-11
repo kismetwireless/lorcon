3c19c7204a5da16696e5df95fffd061db6ba35fa
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-07-11 11:31:25 -0400

    Use native endian for rtap length

--- lorcon_packet.c
+++ lorcon_packet.c
@@ -524,7 +524,8 @@ int lorcon_packet_txprep_by_ctx(lorcon_t *context, lorcon_packet_t *packet,
 
 		rtap_hdr->it_version = 0;
 		rtap_hdr->it_pad = 0;
-		rtap_hdr->it_len = lorcon_le16(sizeof(struct lorcon_inject_radiotap_header));
+		// rtap_hdr->it_len = lorcon_le16(sizeof(struct lorcon_inject_radiotap_header));
+		rtap_hdr->it_len = sizeof(struct lorcon_inject_radiotap_header);
 #if 0
 		rtap_hdr->it_present =
 			lorcon_le32((1 << TX_IEEE80211_RADIOTAP_FLAGS) | 
